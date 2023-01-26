d02bcd2fa700a5c2663f01727bd56d9cd7274f0c
Lihanbyeol <123554115+Lihanbyeol@users.noreply.github.com>
2023-01-26 21:00:13 +0900

    Time: 26 ms (32.82%), Space: 42.6 MB (82.60%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public String convert(String s, int numRows) {
+		String rslt = "";
+
+		if(numRows == 1) {
+			return s;
+		}
+        
+		List<List<String>> info = new ArrayList<>();
+		for (int i = 0; i < numRows; i++) {
+			info.add(new ArrayList<>());
+		}
+
+		String[] sArr = s.split("");
+
+		int idx = 0; // info의 인덱스
+		boolean flag = false; // false : + , true : -
+		for (int i = 0; i < sArr.length; i++) {
+			if (!flag) {
+				info.get(idx).add(sArr[i]);
+				if(idx < numRows - 1) {
+					idx++;
+				} else {
+					flag = true;
+					idx--;
+				}
+			} else {
+				info.get(idx).add(sArr[i]);
+				if(idx > 0) {
+					idx--;
+				} else {
+					flag = false;
+					idx++;
+				}
+			}
+		}
+		
+		for(int i = 0; i < numRows; i++) {
+			for(int j = 0; j < info.get(i).size(); j++) {
+				rslt += info.get(i).get(j);
+			}
+		}
+
+
+		return rslt;        
+    }
+}
