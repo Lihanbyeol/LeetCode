9d508d48edf08effb24dfe345804e10dd2d0f77c
Lihanbyeol <123554115+Lihanbyeol@users.noreply.github.com>
2023-01-30 21:41:24 +0900

    Time: 4 ms (72.93%), Space: 43 MB (40.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,19 +2,24 @@ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] rslt = new int[2];
-
-        label : for(int i = 0; i < nums.length; i++) {
-        	int first = nums[i];
-        	for(int j = i+1; j < nums.length; j++) {
-        		int second = nums[j];
-        		if(first + second == target) {
-        			rslt[0] = i;
-        			rslt[1] = j;
-        			break label;
-        		}
-        	}
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+        	map.put(nums[i], i);
         }
         
+        for(int i = 0; i < nums.length; i++) {
+        	
+        	if(map.containsKey(target - nums[i])
+        			&& i != map.get(target - nums[i])) {
+        		int aIdx = i;
+        		int bIdx = map.get(target - nums[i]);
+        		rslt[0] = aIdx;
+        		rslt[1] = bIdx;
+        		Arrays.sort(rslt);
+        		return rslt;
+        	}
+        }
         return rslt;
     }
 }
