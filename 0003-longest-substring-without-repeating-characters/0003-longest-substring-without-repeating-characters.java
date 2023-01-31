20eb26774ff137349e2308faeb5784e9965e36fa
Lihanbyeol <123554115+Lihanbyeol@users.noreply.github.com>
2023-02-01 01:46:34 +0900

    Time: 5 ms (93.12%), Space: 42.2 MB (93.30%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       int rslt = 0;
+        int rslt = 0;
         
         Map<Character, Integer> map = new HashMap<>();
 
-        int len = 0;
+        int strtIdx = 0;
         for(int i = 0; i < s.length(); i++) {
         	char c = s.charAt(i);
         	
-        	// 이미 존재한다면
-        	if(map.containsKey(c)) {
-        		// 이전 결과값과 이번 substirng의 길이 비교하여 더 큰 값을 결과값으로 
-        		rslt = Math.max(rslt, len);
+        	// substring의 시작 인덱스 이후에 해당 문자가 이미 존재한다면
+        	if(map.containsKey(c) && map.get(c) >= strtIdx ) {
+        		rslt = Math.max(rslt, i - strtIdx);
         		
-        		// 이전 인덱스 다음부터 다시 진행
-        		i = map.get(c);
-        		map.clear();
-        		len = 0;
-        		continue;
-        	} else {
-        		len++;
+        		strtIdx = map.get(c) + 1;
         	}
+        	
         	map.put(c, i);
         }
         
-        rslt = Math.max(rslt, len);
+        rslt = Math.max(rslt, s.length() - strtIdx);
 
         return rslt;
     }
