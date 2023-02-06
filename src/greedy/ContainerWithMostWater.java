ef9588a513a3ea32eb57ac1a32449f574bbbd118
gksquf4056 <gksquf4056@gmail.com>
2023-02-06 21:53:08 +0900

    Leet Code 11. Container With Most Water

---
+++ src/greedy/ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package greedy;
+
+public class ContainerWithMostWater {
+	public int maxArea(int[] height) {
+		int rslt = 0;
+
+		if (height.length <= 1) {
+			return rslt;
+		}
+
+		int left = 0;
+		int right = height.length - 1;
+		
+		while(left < right) {
+			int x = right - left;
+			int y = Math.min(height[left], height[right]);
+			rslt = Math.max(rslt, x*y);
+			
+			if(height[left] > height[right]) {
+				right--;
+			}
+			else {
+				left++;
+			}
+		}
+		
+		return rslt;
+	}
+}
